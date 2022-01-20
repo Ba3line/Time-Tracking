b02d2bfe442d4fc038d8aef44d11df2c6f9262da
el faquiri ismail <ba3line@MacBook-Pro-de-Ba3line.local>
2022-01-20 10:57:05 +0100

    time-tracking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
+import User from './component/User/user'
+import CardMini from './component/cardMini/CardMini'
+import data from './data.json'
+import './component/cardMini/CardMini.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [monState, setMonState] = useState("weekly")
+
+  const daily = () => {
+    setMonState("daily")
+  }
+
+  const weekly = () => {
+    setMonState("weekly")
+  }
+
+  const monthly = () => {
+    setMonState("monthly")
+  }
+
+  const couleur = ["bg-light", "bg-softBlue", "bg-lightRed", "bg-limeGreen", "bg-violet", "bg-softOrange"]
+
+  const hover = ["nbrHeuresWork", "nbrHeuresPlay", "nbrHeuresStudy" , "nbrHeuresExercices",  "nbrHeuresSocial", "nbrHeuresSelf"]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="toutecard">
+        <User daily={daily} weekly={weekly} monthly={monthly} />
+        <div className="allCards">
+          <div className="raw1">
+            {data.slice(0, 3).map((data) => (<CardMini titre={data.title} heure={data.timeframes[monState].current} heure2={data.timeframes[monState].previous} bgcolor={couleur[data.id - 1]} svg={data.id - 1} hover={hover[data.id - 1]} />))}
+          </div>
+          <div className="raw2">
+            {data.slice(3, 6).map((data) => (<CardMini titre={data.title} heure={data.timeframes[monState].current} heure2={data.timeframes[monState].previous} bgcolor={couleur[data.id - 1]} svg={data.id - 1} hover={hover[data.id - 1]} />))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
